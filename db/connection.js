afba7262af8a77cf8a8cbb67702eef6412cb8c85
Iffa Hossain <iffa26@users.noreply.github.com>
2021-04-18 17:12:51 +0100

    debug connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,14 +3,17 @@ const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
 
 const dbConfig =
-  ENV === "production"
-    ? { client: "pg", 
-      connectionString: process.env.DATABASE_URL, 
-      ssl: {
-        rejectUnauthorized : false,
-      },
-    } 
-  : {};
+  ENV === 'production'
+    ? { 
+        client: 'pg', 
+        connection: { 
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        }, 
+      }
+    : require('../knexfile');
 
 const connection = knex(dbConfig);
 
